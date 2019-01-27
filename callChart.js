30a0694a3f657001cf9326947372c478f6937306
Timmy Nielsen <timmy.j.nielsen@gmail.com>
2019-01-27 19:40:37 +0100

    Version 1

---
+++ callChart.js
@@ -0,0 +1,80 @@
+// Render a new chart
+function callChart(skillSet){
+    d3.csv('Bok.csv', function(error, data) {
+      if (error) {
+          console.error('Error getting or parsing the data.');
+          throw error;
+      }
+    var groups = [
+        {
+            nr: "Group nr 1",
+            members: [],
+            totalSkill: 0
+        },
+        {
+            nr: "Group nr 2",
+            members: [],
+            totalSkill: 0
+        },
+        {
+            nr: "Group nr 3",
+            members: [],
+            totalSkill: 0
+        },
+        {
+            nr: "Group nr 4",
+            members: [],
+            totalSkill: 0
+        },
+        {
+            nr: "Group nr 5",
+            members: [],
+            totalSkill: 0
+        },
+        {
+            nr: "Group nr 6",
+            members: [],
+            totalSkill: 0
+        },
+        {
+            nr: "Group nr 7",
+            members: [],
+            totalSkill: 0
+        },
+        {
+            nr: "Group nr 8",
+            members: [],
+            totalSkill: 0
+        },
+        {
+            nr: "Group nr 9",
+            members: [],
+            totalSkill: 0
+        },
+        {
+            nr: "Group nr 10",
+            members: [],
+            totalSkill: 0
+        }
+    ]
+    
+    let skill = skillSet;
+    let leastSkilledGroup = 0;
+    data.sort(function(a, b){return a[skill]-b[skill]});
+    for(let i = 0; i < 57; i++){
+        for(let j = 0; j < 10; j++){
+            if(groups[j].totalSkill < groups[leastSkilledGroup].totalSkill){
+                leastSkilledGroup = j;
+            }
+        }
+        groups[leastSkilledGroup].members.push(data[i].Name);
+        groups[leastSkilledGroup].totalSkill += +data[i][skill];
+    }
+
+    var chart = bubbleChart().width(1200).height(600).title("Group based on " + skill);      
+    chart.columnForTitle("members").columnForColors("nr").columnForRadius("totalSkill").unitName("Total " + skill + ":");
+
+    // selection.datum() returns the bound datum for the first element in the selection and doesn't join the specified array of data with the selected elements
+    d3.select('#chart').datum(groups).call(chart);
+    });
+}
---
+++ studentsChart.js
@@ -0,0 +1,39 @@
+// Render a new chart
+function studentsChart(){
+    d3.csv('Bok.csv', function(error, data) {
+      if (error) {
+          console.error('Error getting or parsing the data.');
+          throw error;
+      }
+
+    skillArray = ['VisSkills','StatSkills','MathSkills','ArtSkills','CompSkills','CodeSkills','GraphSkills','MDISkills','EvalSkills','CommSkills','CollabSkills','RepoSkills']
+
+    for(var i = 0; i < data.length; i++){
+        var maxLevel = 0;
+        var totalSkillTemp = 0;
+        var bestSkillTemp = "VisSkills";
+       
+        for(var j = 0; j < 12; j++){
+            totalSkillTemp += +data[i][skillArray[j]];
+            if(data[i][skillArray[j]] > maxLevel){
+                maxLevel = data[i][skillArray[j]];
+                bestSkillTemp = skillArray[j];
+            }
+        }
+        data[i].totalSkill = totalSkillTemp;
+        data[i].bestSkill = bestSkillTemp;
+        data[i].bestSkillValue = data[i][bestSkillTemp];
+        maxLevel = 0;
+        totalSkill = 0;
+    }
+
+    console.log(data);
+    var chart = bubbleChart().width(1200).height(1000).title("All students");      
+    chart.totalSkill("bestSkillValue").columnForTitle("Name").columnForColors("bestSkill").columnForRadius("totalSkill").unitName("Total skill level:");
+
+    // selection.datum() returns the bound datum for the first element in the selection and doesn't join the specified array of data with the selected elements
+    d3.select('#chart').datum(data).call(chart);
+
+
+});
+}
